508946707c26d6feabd0d3bac06102b5f8c954e3
teacherfiallo-project <teacherfiallo@gmail.com>
2026-03-30 00:05:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,3 +43,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
+app.get("/", (req, res) => {
+  res.send("Career English Pro API is running 🚀");
+});
