51e914968ea586ba958faa046426f8e0f6fd2699
teacherfiallo-project <teacherfiallo@gmail.com>
2026-03-30 00:32:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post("/chat", async (req, res) => {
 
 // Rota de teste (MUITO IMPORTANTE)
 app.get("/", (req, res) => {
-  res.send("Career English Pro API is running 🚀");
+  res.status(200).send("OK");
 });
 
 // Porta dinâmica do Railway
